355d61c178e909acbc203c0a961e870491162300
Jisoo Shin <jisooxshin@gmail.com>
2016-01-27 15:26:21 -0500

    showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -22,15 +22,32 @@ router.get('/add', function(req, res, next) {
   res.render('addpage');
 });
 
+router.get('/:urlTitle', function(req, res, next) {
+	var pageName = req.params.urlTitle;
+	// res.send('hit dynamic route at ' + req.params.urlTitle);
+	Page.findOne({ 'urlTitle': pageName })
+	.populate('author')
+	.then(function(page) {
+		console.log(page);
+		res.render('wikipage', page);
+	}, function(err) {console.log(err)})
+});
+
 router.post('/', function(req, res, next) {
 	var page = new Page ({
 		title: req.body.title,
-		content: req.body.content
+		content: req.body.content,
+		// author: req.body.name
 	});
 	page.save()
-	.then(function() {
-		res.redirect('/');
+	.then(function(page) {
+		res.redirect(page.route);
 	}, function(err) { console.log(err)})
 });
 
 module.exports = router;
+
+
+
+
+
